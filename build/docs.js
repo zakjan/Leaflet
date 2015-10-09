1513af6f2d584755c98c3b466184c35c366fab8c
Iván Sánchez Ortega <ivan@mazemap.no>
2015-10-09 14:36:05 +0200

    🍂doc: miniclasses for event types, bump to Leafdoc 1.0.0

--- build/docs.js
+++ build/docs.js
@@ -9,6 +9,10 @@ function buildDocs() {
 		showInheritancesWhenEmpty: true
 	});
 
+	// Note to Vladimir: Iván's never gonna uncomment the following line. He's
+	// too proud of the little leaves around the code.
+	//doc.setLeadingChar('@');
+
 	// Leaflet uses a couple of non-standard documentable things. They are not
 	// important enough to be classes/namespaces of their own, and should
 	// just be listed in a table like the rest of documentables:
@@ -22,96 +26,16 @@ function buildDocs() {
 
 
 	/// TODO: Review sections in Map.js, maybe move methods around
-// 	doc.addFile('src/map/Map.js');
-
-	/// TODO: Options for map methods!! (fitbounds opts, zoom-pan opts, etc)
-	/// Maybe implement the "minor classes" thing?
-
-
-// 	doc.addFile('src/map/anim/Map.PanAnimation.js');
-// 	doc.addFile('src/map/anim/Map.ZoomAnimation.js');
-// 	doc.addFile('src/map/anim/Map.FlyTo.js');
-// 	doc.addFile('src/map/ext/Map.Geolocation.js');
-// 	doc.addFile('src/map/handler/Map.BoxZoom.js');
-// 	doc.addFile('src/map/handler/Map.DoubleClickZoom.js');
-// 	doc.addFile('src/map/handler/Map.Drag.js');
-// 	doc.addFile('src/map/handler/Map.Keyboard.js');
-// 	doc.addFile('src/map/handler/Map.ScrollWheelZoom.js');
-// 	doc.addFile('src/map/handler/Map.Tap.js');
-// 	doc.addFile('src/map/handler/Map.TouchZoom.js');
-//
-// 	doc.addFile('src/layer/marker/Marker.js');
-// 	doc.addFile('src/layer/marker/Marker.Drag.js');
-// 	doc.addFile('src/layer/marker/Marker.Popup.js');
-// 	doc.addFile('src/layer/Popup.js');
-//
-// 	doc.addFile('src/layer/tile/TileLayer.js');
-// 	doc.addFile('src/layer/tile/TileLayer.WMS.js');
-// 	doc.addFile('src/layer/ImageOverlay.js');
-//
-// 	doc.addFile('src/layer/vector/Path.js');
-// 	doc.addFile('src/layer/vector/Polyline.js');
-// 	doc.addFile('src/layer/vector/Polygon.js');
-// 	doc.addFile('src/layer/vector/Rectangle.js');
-// 	doc.addFile('src/layer/vector/Circle.js');
-// 	doc.addFile('src/layer/vector/CircleMarker.js');
-//
-// 	doc.addFile('src/layer/LayerGroup.js');
-// 	doc.addFile('src/layer/FeatureGroup.js');
-// 	doc.addFile('src/layer/GeoJSON.js');
-// 	doc.addFile('src/layer/tile/GridLayer.js');
-//
-// 	doc.addFile('src/geo/LatLng.js');
-// 	doc.addFile('src/geo/LatLngBounds.js');
-// 	doc.addFile('src/geometry/Point.js');
-// 	doc.addFile('src/geometry/Bounds.js');
-// 	doc.addFile('src/layer/marker/Icon.js');
-// 	doc.addFile('src/layer/marker/DivIcon.js');
-//
-// 	doc.addFile('src/control/Control.Zoom.js');
-// 	doc.addFile('src/control/Control.Attribution.js');
-// 	doc.addFile('src/control/Control.Layers.js');
-// 	doc.addFile('src/control/Control.Scale.js');
-//
-// 	doc.addFile('src/core/Browser.js');
-// 	doc.addFile('src/core/Util.js');
-// 	doc.addFile('src/geometry/Transformation.js');
-// 	doc.addFile('src/geometry/LineUtil.js');
-// 	doc.addFile('src/geometry/PolyUtil.js');
-// 	doc.addFile('src/dom/DomEvent.js');
-// 	doc.addFile('src/dom/DomEvent.DoubleTap.js');
-// 	doc.addFile('src/dom/DomEvent.Pointer.js');
-// 	doc.addFile('src/dom/DomUtil.js');
-// 	doc.addFile('src/dom/PosAnimation.js');
-// 	doc.addFile('src/dom/Draggable.js');
-//
-// 	doc.addFile('src/core/Class.js');
-// 	doc.addFile('src/core/Events.js');
-// 	doc.addFile('src/layer/Layer.js');
-// 	doc.addFile('src/layer/Layer.Popup.js');
-// 	doc.addFile('src/control/Control.js');
-// 	doc.addFile('src/core/Handler.js');
-// 	doc.addFile('src/geo/projection/*.js');
-// 	doc.addFile('src/geo/crs/CRS.js');
 
 	/// FIXME: Event objects
 	/// Maybe implement the "minor classes" thing?
 
 
-	// console.log('internal namespaces are', doc._namespaces);
-
-
-	// console.log('status is', doc);
-
-	// console.log('calling outputStr');
 	var out = doc.outputStr();
-
 
 	var fs = require('fs');
 
-
 	fs.writeFileSync('dist/reference.html', out);
-
 }
 
 module.exports = buildDocs;
